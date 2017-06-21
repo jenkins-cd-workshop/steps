2909dce9ac91d3fc945ca4ba50ccd789118cd50b
Giorgio Sironi <g.sironi@elifesciences.org>
2017-06-21 15:10:04 +0100

    Bugfixes for step repository
    - script must be executable
    - if condition does not need [] wrapping

--- push-into-jenkins.sh
+++ push-into-jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-if [ ! git remote | grep local-jenkins ]; then
+if ! git remote | grep local-jenkins; then
     git remote add local-jenkins ssh://localhost:16022/workflowLibs.git
 fi
 git push local-jenkins master
